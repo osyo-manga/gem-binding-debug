89c06f8d647a2e01f72fd8c2a284b3e41e202ceb
manga_osyo <manga.osyo@gmail.com>
2019-09-08 13:30:28 +0900

    Change indent style.

--- lib/binding/debug.rb
+++ lib/binding/debug.rb
@@ -1,42 +1,42 @@
 require "binding/debug/version"
 
 module BindingDebug
-	module Formats
-		module_function
-		def default
-			proc { |name, value| "#{name} : #{value}" }
-		end
+  module Formats
+    module_function
+    def default
+      proc { |name, value| "#{name} : #{value}" }
+    end
 
-		def inspect formatter = default
-			proc { |name, value| formatter.call "#{name}.inspect", value.inspect }
-		end
+    def inspect formatter = default
+      proc { |name, value| formatter.call "#{name}.inspect", value.inspect }
+    end
 
-		def prefix str
-			proc { |name, value| "#{str} #{BindingDebug::Formats.default.call name, value}" }
-		end
+    def prefix str
+      proc { |name, value| "#{str} #{BindingDebug::Formats.default.call name, value}" }
+    end
 
-		def suffix str
-			proc { |name, value| "#{BindingDebug::Formats.default.call name, value} #{str}" }
-		end
-	end
+    def suffix str
+      proc { |name, value| "#{BindingDebug::Formats.default.call name, value} #{str}" }
+    end
+  end
 
-	refine Binding do
-		def debug expr, &block
-			block ||= Formats.default
-			binding = self
-			expr.split("\n").reject { |it| /^\s*$/ =~ it }.map(&:strip).map { |expr|
-				block.call(expr, binding.eval(expr))
-			}.join("\n")
-		end
+  refine Binding do
+    def debug expr, &block
+      block ||= Formats.default
+      binding = self
+      expr.split("\n").reject { |it| /^\s*$/ =~ it }.map(&:strip).map { |expr|
+        block.call(expr, binding.eval(expr))
+      }.join("\n")
+    end
 
-		def p expr, &block
-			block ||= Formats.default
-			puts(expr){ |name, value| block.call name, value.inspect }
-			expr
-		end
+    def p expr, &block
+      block ||= Formats.default
+      puts(expr){ |name, value| block.call name, value.inspect }
+      expr
+    end
 
-		def puts expr, &block
-			super debug expr, &block
-		end
-	end
+    def puts expr, &block
+      super debug expr, &block
+    end
+  end
 end
--- lib/binding/debug/version.rb
+++ lib/binding/debug/version.rb
@@ -1,3 +1,3 @@
 module BindingDebug
-	VERSION = "0.2.0"
+  VERSION = "0.2.0"
 end
